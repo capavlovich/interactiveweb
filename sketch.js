24599d667563e509db92039de494a397506595d9
capavlovich <98490505+capavlovich@users.noreply.github.com>
2023-05-08 09:49:28 -0700

    uploading pages & images

--- sketch.js
+++ sketch.js
@@ -3,6 +3,8 @@ let y = 0;
 let xpos;
 let ypos;
 
+let myWindow;
+
 let mode = 1;
 
 // let bear = document.getElementById('bear');
@@ -22,90 +24,40 @@ function setup() {
 
     
 
-    //   if ((xpos>800 && xpos<1100) && (ypos>600 && ypos<900)){
-    //     //make bear appear
-    //     // bear.style.display='block';
-    //   }
+      if ((xpos>800 && xpos<1100) && (ypos>600 && ypos<900)){
+        //make bear appear
+        // bear.style.display='block';
+      }
 
-    //   if ((xpos<400) && (ypos<400)){
+      if ((xpos<400) && (ypos<400)){
 
-    //     //hide bear again
-    //     // bear.style.display='none';
+        //hide bear again
+        // bear.style.display='none';
 
-    //     //change scene to the next to the next one by adding 1
-    //     mode = 2;
+        //change scene to the next to the next one by adding 1
+        mode = 2;
 
-    //     console.log(mode)
-    //   }
+        console.log(mode)
+      }
 
-      // //changing of scenes
-    //   if (mode == 2){
-    //     removeElements();
-    //     background(0);
-    //     bear.style.display='none';
-    //   }
-    // }
+      //changing of scenes
+      if (mode == 2){
+        removeElements();
+        //background(0);
+        bear.style.display='none';
+      }
+    }
     function windowResized() {
         // resize the canvas when the screen is resized.
         resizeCanvas(windowWidth, windowHeight);
     }
     
     
-    function displayGoat(){
-        removeElements();
-        //lets manipulate the DOM and make our cat move with the arrow keys
-        //create an image element in html
-        let goat = createImg("images/flowergoat.png");
-        //select size of image
-        goat.size(200,200);
-         // give it an id
-        goat.id('goat');
-    
-        //set the position
-        //create arithmetic for x and y position. this lets us read out the position
-        xpos = width/2-100+x;
-        ypos = height/2-100+y;
-    
-      
-        //position the cat based on the arrow keys
-        goat.position(xpos, ypos);
-    
-    
-    //controls using arrow keys
-        if (keyIsPressed){
-          if(keyCode == UP_ARROW){
-    
-            //minus 5 in the y direction means up
-            y-=5;
-            
-          } 
-          
-          if(keyCode == RIGHT_ARROW) {
-    
-            //plus 5 in the x direction means right
-            x+=5;
-          }
-    
-          if(keyCode == DOWN_ARROW) {
-    
-            //plus 5 in the y direction means down
-            y+=5;
-          }
-    
-          if(keyCode == LEFT_ARROW) {
-    
-            //minus 5 in the x direction means left
-            x-=5;
-          }
-      }
-    }
-    
-    var video = document.getElementById("myVideo");
-    var btn = document.getElementById("myBtn");
+
 //   function windowResized() {
-//     // resize the canvas when the screen is resized.
+// //     // resize the canvas when the screen is resized.
 //     resizeCanvas(windowWidth, windowHeight);
-}
+// }
 
 function windowResized() {
     // resize the canvas when the screen is resized.
@@ -117,7 +69,7 @@ function displayGoat(){
     removeElements();
     //lets manipulate the DOM and make our cat move with the arrow keys
     //create an image element in html
-    let goat = createImg("images/goat.png");
+    let goat = createImg('images/flowergoat.png');
     //select size of image
     goat.size(200,200);
      // give it an id
@@ -138,26 +90,30 @@ function displayGoat(){
       if(keyCode == UP_ARROW){
 
         //minus 5 in the y direction means up
-        y-=5;
+        y-=2;
         
       } 
       
       if(keyCode == RIGHT_ARROW) {
 
         //plus 5 in the x direction means right
-        x+=5;
+        x+=7;
       }
 
       if(keyCode == DOWN_ARROW) {
 
         //plus 5 in the y direction means down
-        y+=5;
+        y+=2;
       }
 
       if(keyCode == LEFT_ARROW) {
 
         //minus 5 in the x direction means left
-        x-=5;
+        x-=7;
       }
   }
+}
+
+function openWin() {
+  myWindow = window.open("", "", "width=5760, height=1080");
 }
